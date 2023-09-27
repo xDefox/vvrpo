5876ecb0ea4c6c3716f8d8b4f2ef58df8e426e9b
xDefox <celerval5@gmail.com>
2023-09-27 09:08:24 +0300

    Добавьте файлы проекта.

---
+++ lab6x/lab6x.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    
+    double x = 0, f,h;
+    int a, b;
+
+    //cout << "Введите X: ";
+    //cin >> x;
+    
+    cout << "Введите пределы a, b, и шаг h: ";
+        cin >> a >> b >> h;
+        cout << "x" << "\t" << "y" << endl;
+
+        for (double x = a; x <= b ; x += h) {
+            
+            if (x <= -1)
+                f = x * (-1);
+            else if (x <= 1)
+                f = 3 * pow(x, 2);
+            else
+                f = 1;
+            
+            cout << x << "\t" << f << endl;
+
+        }
+    
+}
